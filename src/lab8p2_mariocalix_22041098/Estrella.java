5d9c8a62bc3cd363ec2da6b311e4cd0e208e3684
caalix <mariocalix02@gmail.com>
2021-12-03 13:30:28 -0600

    Clase jugador estrella partida

---
+++ src/lab8p2_mariocalix_22041098/Estrella.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab8p2_mariocalix_22041098;
+
+/**
+ *
+ * @author mario
+ */
+public class Estrella {
+    
+}
