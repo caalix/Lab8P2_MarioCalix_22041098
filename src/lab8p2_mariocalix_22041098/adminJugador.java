431f33f01e1f96d465243571daab25630aceb459
caalix <mariocalix02@gmail.com>
2021-12-03 13:58:59 -0600

    clase adminJugador

---
+++ src/lab8p2_mariocalix_22041098/adminJugador.java
@@ -0,0 +1,85 @@
+
+package lab8p2_mariocalix_22041098;
+
+import java.io.EOFException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+
+public class adminJugador {
+    
+    private ArrayList<Jugador> listaJugadores = new ArrayList();
+    private File archivo = null;
+    
+    public adminJugador(String path) {
+        archivo = new File(path);
+    }
+
+    public ArrayList<Jugador> getListaJugadores() {
+        return listaJugadores;
+    }
+
+    public void setListaJugadores(ArrayList<Jugador> listaJugadores) {
+        this.listaJugadores = listaJugadores;
+    }
+
+    public File getArchivo() {
+        return archivo;
+    }
+
+    public void setArchivo(File archivo) {
+        this.archivo = archivo;
+    }
+    
+    public void setJugador(Jugador a){
+        listaJugadores.add(a);
+    }
+    
+    
+    public void cargarArchivo() {
+        try {            
+            listaJugadores = new ArrayList();
+            Jugador temp;
+            if (archivo.exists()) {
+                FileInputStream entrada
+                    = new FileInputStream(archivo);
+                ObjectInputStream objeto
+                    = new ObjectInputStream(entrada);
+                try {
+                    while ((temp = (Jugador) objeto.readObject()) != null) {
+                        listaJugadores.add(temp);
+                    }
+                } catch (EOFException e) {
+                    //encontro el final del archivo
+                }
+                objeto.close();
+                entrada.close();
+            }            
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public void escribirArchivo() {
+        FileOutputStream fw = null;
+        ObjectOutputStream bw = null;
+        try {
+            fw = new FileOutputStream(archivo);
+            bw = new ObjectOutputStream(fw);
+            for (Jugador t : listaJugadores) {
+                bw.writeObject(t);
+            }
+            bw.flush();
+        } catch (Exception ex) {
+        } finally {
+            try {
+                bw.close();
+                fw.close();
+            } catch (Exception ex) {
+            }
+        }
+    }
+}
